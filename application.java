a6d314e84988a5d6f2972d97bacd827b448eeffa
Sathishsamala12 <sathishsamala12@gmail.com>
2021-12-07 15:42:47 +0530

    new changes done for feature302 by dev2

--- application.java
+++ application.java
@@ -17,4 +17,19 @@ public class Main {
     else
       System.out.println(num + " is not a prime number.");
   }
+#new changes done by dev2
+  public class Alphabet {
+
+    public static void main(String[] args) {
+
+        char c = 'A';
+
+        String output = (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+                ? c + " is an alphabet."
+                : c + " is not an alphabet.";
+
+        System.out.println(output);
+    }
+}
+
 }
